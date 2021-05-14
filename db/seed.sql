7678a82e7e0eab82e6e024ca7e15387a6ec8b7a2
Steven Clark <stevenclark@steb.localdomain>
2021-05-14 09:26:35 -0600

    initial commit

---
+++ db/seed.sql
@@ -0,0 +1,2 @@
+create table helo_users (id serial primary key, username varchar not null, password varchar not null, profile_pic text);
+create table helo_posts (id serial primary key, title varchar(45) not null, content text, img text, author_id integer references helo_users(id), date_created timestamp);
--- server/index.js
+++ server/index.js
@@ -1,23 +1,41 @@
-require('dotenv').config();
-const express = require('express'),
-      userCtrl = require('./controllers/user'),
-      postCtrl = require('./controllers/posts')
+require("dotenv").config();
+const massive = require("massive");
+const session = require("express-session");
 
+const express = require("express"),
+  userCtrl = require("./controllers/user"),
+  postCtrl = require("./controllers/posts");
 
 const app = express();
 
+const { SERVER_PORT, CONNECTION_STRING, SESSION_SECRET } = process.env;
+
 app.use(express.json());
+app.use(
+  session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 },
+  })
+);
+
+massive({
+  connectionString: CONNECTION_STRING,
+}).then((db) => {
+  app.set("db", db);
+  console.log("database is connnected");
+  app.listen(SERVER_PORT, (_) => console.log(`running on ${SERVER_PORT}`));
+});
 
 //Auth Endpoints
-app.post('/api/auth/register', userCtrl.register);
-app.post('/api/auth/login', userCtrl.login);
-app.get('/api/auth/me', userCtrl.getUser);
-app.post('/api/auth/logout', userCtrl.logout);
+app.post("/api/auth/register", userCtrl.register);
+app.post("/api/auth/login", userCtrl.login);
+app.get("/api/auth/me", userCtrl.getUser);
+app.post("/api/auth/logout", userCtrl.logout);
 
 //Post Endpoints
-app.get('/api/posts', postCtrl.readPosts);
-app.post('/api/post', postCtrl.createPost);
-app.get('/api/post/:id', postCtrl.readPost);
-app.delete('/api/post/:id', postCtrl.deletePost)
-
-app.listen(4000, _ => console.log(`running on ${4000}`));
+app.get("/api/posts", postCtrl.readPosts);
+app.post("/api/post", postCtrl.createPost);
+app.get("/api/post/:id", postCtrl.readPost);
+app.delete("/api/post/:id", postCtrl.deletePost);
